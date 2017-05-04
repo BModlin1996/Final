c2d769972d4bdfa0b58fd33fb26a9b6b673504de
Bailey <baileymodlin@baileys-macbook-pro.local>
2017-05-03 22:31:52 -0400

    Update 5/03/2017 - App displays data from ODBII to app gui

--- app/src/main/java/com/example/baileymodlin/afinal/CheckEngineActivity.java
+++ app/src/main/java/com/example/baileymodlin/afinal/CheckEngineActivity.java
@@ -3,8 +3,10 @@ package com.example.baileymodlin.afinal;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -36,10 +38,10 @@ import java.util.UUID;
 public class CheckEngineActivity extends AppCompatActivity {
 
     private BluetoothSocket socket;
-    private ArrayList devices;
-    private ArrayList deviceString;
-    private String deviceAddress;
     private EditText celCode;
+    private Context context;
+    private String cel;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,81 +50,127 @@ public class CheckEngineActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.checkEngineToolbar);
         celCode = (EditText) findViewById(R.id.celEditText);
         setSupportActionBar(toolbar);
-        setUpBluetoothDevice();
-        connectBluetoothDevice();
-        getData();
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+        handler = new Handler();
 
-    }
-
-
-    private  void  setUpBluetoothDevice(){
-        //Connect Bluetooth device
-        ArrayList deviceString = new ArrayList();
-        final ArrayList devices = new ArrayList();
-
-        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
-        if(pairedDevices.size() > 0){
-            for (BluetoothDevice device : pairedDevices) {
-                deviceString.add(device.getName() + "\n" + device.getAddress());
-                devices.add(device.getAddress());
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                setUpBluetoothDevice();
+                connectBluetoothDevice();
+                getData();
             }
 
-        }
-        final AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.select_dialog_singlechoice, deviceString.toArray(new String[deviceString.size()]));
-        alert.setSingleChoiceItems(arrayAdapter, -1, new DialogInterface.OnClickListener(){
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
+            private  void  setUpBluetoothDevice(){
+                //Connect Bluetooth device
+                ArrayList deviceString = new ArrayList();
+                final ArrayList devices = new ArrayList();
+
+                BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+                Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+                if(pairedDevices.size() > 0){
+                    for (BluetoothDevice device : pairedDevices) {
+                        deviceString.add(device.getName() + "\n" + device.getAddress());
+                        devices.add(device.getAddress());
+                    }
+
+                }
+                //final AlertDialog.Builder alert = new AlertDialog.Builder(context);
+                ArrayAdapter arrayAdapter = new ArrayAdapter(context, android.R.layout.select_dialog_singlechoice, deviceString.toArray(new String[deviceString.size()]));
+                /**
+                 alert.setSingleChoiceItems(arrayAdapter, -1, new DialogInterface.OnClickListener(){
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
                 dialogInterface.dismiss();
                 int pos = ((AlertDialog) dialogInterface).getListView().getCheckedItemPosition();
                 deviceAddress = (String) devices.get(pos);
+                }
+                });
+
+                 alert.setTitle("Choose Bluetooth device");
+                 alert.show();
+                 **/
             }
-        });
 
-        alert.setTitle("Choose Bluetooth device");
-        alert.show();
+            private void connectBluetoothDevice(){
+
+                //Save Bluetooth device
+                BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+                BluetoothDevice device = btAdapter.getRemoteDevice("00:1D:A5:00:F7:61");
+
+                UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+                try{
+                    socket = device.createInsecureRfcommSocketToServiceRecord(uuid);
+                    socket.connect();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    try {
+                        socket.close();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+
+            }
+
+            public void getData(){
+                try {
+                    new EchoOffCommand().run(socket.getInputStream(), socket.getOutputStream());
+                    new LineFeedOffCommand().run(socket.getInputStream(), socket.getOutputStream());
+                    new SelectProtocolCommand(ObdProtocols.AUTO).run(socket.getInputStream(), socket.getOutputStream());
+                    //Creating a new thread for the obd to update too
+                    Thread thread = new Thread();
+                    //Changed 5-2-2017
+
+                    TroubleCodesCommand troubleCodesCommand = new TroubleCodesCommand();
+
+                    while (!thread.currentThread().isInterrupted()) {
+
+                        troubleCodesCommand.run(socket.getInputStream(), socket.getOutputStream());
+
+                        Log.d("DEBUG_0", "Trouble codes: " + troubleCodesCommand.getFormattedResult());
+
+                        // Log.d("DEBUG_3", "Boost/Vac: " + pressureCommand.getFormattedResult());
+                        cel = troubleCodesCommand.getFormattedResult();
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                celCode.setText(cel);
+                            }
+                        });
+
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        new Thread(runnable).start();
+
+
     }
 
-    private void connectBluetoothDevice(){
-        //Save Bluetooth device
-        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        BluetoothDevice device = btAdapter.getRemoteDevice("00:1D:A5:00:F7:61");
 
-        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-
-        try{
-            socket = device.createInsecureRfcommSocketToServiceRecord(uuid);
-            socket.connect();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void getData(){
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
         try {
-            Thread thread = new Thread();
-            new EchoOffCommand().run(socket.getInputStream(), socket.getOutputStream());
-            new LineFeedOffCommand().run(socket.getInputStream(), socket.getOutputStream());
-            new SelectProtocolCommand(ObdProtocols.AUTO).run(socket.getInputStream(), socket.getOutputStream());
-            //Trouble codes
-            TroubleCodesCommand troubleCodesCommand = new TroubleCodesCommand();
-            while (!thread.currentThread().isInterrupted()) {
-
-                Log.d("DEBUG_0", "RPM: " + troubleCodesCommand.getFormattedResult());
-
-
-            }
-
+            socket.close();
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
+
     }
 
     @Override
--- app/src/main/java/com/example/baileymodlin/afinal/DataLogActivity.java
+++ app/src/main/java/com/example/baileymodlin/afinal/DataLogActivity.java
@@ -1,10 +1,15 @@
 package com.example.baileymodlin.afinal;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,10 +21,13 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 
+import com.github.pires.obd.commands.SpeedCommand;
 import com.github.pires.obd.commands.engine.RPMCommand;
 import com.github.pires.obd.commands.fuel.FuelLevelCommand;
 import com.github.pires.obd.commands.fuel.WidebandAirFuelRatioCommand;
 import com.github.pires.obd.commands.pressure.BarometricPressureCommand;
+import com.github.pires.obd.commands.pressure.IntakeManifoldPressureCommand;
+import com.github.pires.obd.commands.pressure.PressureCommand;
 import com.github.pires.obd.commands.protocol.EchoOffCommand;
 import com.github.pires.obd.commands.protocol.LineFeedOffCommand;
 import com.github.pires.obd.commands.protocol.SelectProtocolCommand;
@@ -40,7 +48,13 @@ public class DataLogActivity extends AppCompatActivity{
     private EditText tempEdit;
     private EditText pressure;
     private String deviceAddress;
-
+    private String af;
+    private String rpm;
+    private String baro;
+    private String fuelPress;
+    private String ait;
+    private Context context;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,99 +69,143 @@ public class DataLogActivity extends AppCompatActivity{
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+        this.context = this;
+        handler = new Handler();
 
-        setUpBluetoothDevice();
-        connectBluetoothDevice();
-        getData();
-    }
-
-    private  void  setUpBluetoothDevice(){
-        //Connect Bluetooth device
-        ArrayList deviceString = new ArrayList();
-        final ArrayList devices = new ArrayList();
-
-        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
-        if(pairedDevices.size() > 0){
-            for (BluetoothDevice device : pairedDevices) {
-                deviceString.add(device.getName() + "\n" + device.getAddress());
-                devices.add(device.getAddress());
-            }
-
-        }
-        final AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.select_dialog_singlechoice, deviceString.toArray(new String[deviceString.size()]));
-        alert.setSingleChoiceItems(arrayAdapter, -1, new DialogInterface.OnClickListener(){
+        Runnable runnable = new Runnable() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.dismiss();
-                int pos = ((AlertDialog) dialogInterface).getListView().getCheckedItemPosition();
-                deviceAddress = (String) devices.get(pos);
+            public void run() {
+                setUpBluetoothDevice();
+                connectBluetoothDevice();
+                getData();
             }
-        });
 
-        alert.setTitle("Choose Bluetooth device");
-        alert.show();
+            private  void  setUpBluetoothDevice(){
+                //Connect Bluetooth device
+                ArrayList deviceString = new ArrayList();
+                final ArrayList devices = new ArrayList();
+
+                BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+                Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+                if(pairedDevices.size() > 0){
+                    for (BluetoothDevice device : pairedDevices) {
+                        deviceString.add(device.getName() + "\n" + device.getAddress());
+                        devices.add(device.getAddress());
+                    }
+
+                }
+                //final AlertDialog.Builder alert = new AlertDialog.Builder(context);
+                ArrayAdapter arrayAdapter = new ArrayAdapter(context, android.R.layout.select_dialog_singlechoice, deviceString.toArray(new String[deviceString.size()]));
+                /**
+                alert.setSingleChoiceItems(arrayAdapter, -1, new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                        int pos = ((AlertDialog) dialogInterface).getListView().getCheckedItemPosition();
+                        deviceAddress = (String) devices.get(pos);
+                    }
+                });
+
+                alert.setTitle("Choose Bluetooth device");
+                alert.show();
+                 **/
+            }
+
+            private void connectBluetoothDevice(){
+
+                //Save Bluetooth device
+                BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+                BluetoothDevice device = btAdapter.getRemoteDevice("00:1D:A5:00:F7:61");
+
+                UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+                try{
+                    socket = device.createInsecureRfcommSocketToServiceRecord(uuid);
+                    socket.connect();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    try {
+                        socket.close();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+
+            }
+
+            public void getData(){
+                try {
+                    new EchoOffCommand().run(socket.getInputStream(), socket.getOutputStream());
+                    new LineFeedOffCommand().run(socket.getInputStream(), socket.getOutputStream());
+                    new SelectProtocolCommand(ObdProtocols.AUTO).run(socket.getInputStream(), socket.getOutputStream());
+                    //Creating a new thread for the obd to update too
+                    Thread thread = new Thread();
+                    //Changed 5-2-2017
+                    final RPMCommand rpmCommand = new RPMCommand();
+                    final WidebandAirFuelRatioCommand airFuelCommand = new WidebandAirFuelRatioCommand();
+                    FuelLevelCommand fuelCommand = new FuelLevelCommand();
+                    AirIntakeTemperatureCommand aitCommand = new AirIntakeTemperatureCommand();
+                    BarometricPressureCommand barometricPressureCommand = new BarometricPressureCommand();
+
+
+                    while (!thread.currentThread().isInterrupted()) {
+
+                        rpmCommand.run(socket.getInputStream(), socket.getOutputStream());
+                        aitCommand.run(socket.getInputStream(), socket.getOutputStream());
+                        fuelCommand.run(socket.getInputStream(), socket.getOutputStream());
+                        airFuelCommand.run(socket.getInputStream(), socket.getOutputStream());
+                        barometricPressureCommand.run(socket.getInputStream(), socket.getOutputStream());
+                       // pressureCommand.run(socket.getInputStream(), socket.getOutputStream());
+
+                        Log.d("DEBUG_0", "RPM: " + rpmCommand.getFormattedResult());
+                        Log.d("DEBUG_1", "Air Fuel: " + airFuelCommand.getFormattedResult());
+                        Log.d("DEBUG_2", "IAT: " + aitCommand.getFormattedResult());
+                       // Log.d("DEBUG_3", "Boost/Vac: " + pressureCommand.getFormattedResult());
+                        af = airFuelCommand.getFormattedResult();
+                        rpm = rpmCommand.getFormattedResult();
+                        fuelPress = fuelCommand.getFormattedResult();
+                        ait = aitCommand.getFormattedResult();
+                        baro = barometricPressureCommand.getFormattedResult();
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                airFuelEdit.setText(af);
+                                rpmEdit.setText(rpm);
+                                fuelEdit.setText(fuelPress);
+                                tempEdit.setText(ait);
+                                pressure.setText(baro);
+                            }
+                        });
+
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        new Thread(runnable).start();
+
     }
 
-    private void connectBluetoothDevice(){
 
-        //Save Bluetooth device
-        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        BluetoothDevice device = btAdapter.getRemoteDevice("00:1D:A5:00:F7:61");
-
-        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-
-        try{
-            socket = device.createInsecureRfcommSocketToServiceRecord(uuid);
-            socket.connect();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public void getData(){
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
         try {
-            new EchoOffCommand().run(socket.getInputStream(), socket.getOutputStream());
-            new LineFeedOffCommand().run(socket.getInputStream(), socket.getOutputStream());
-            new SelectProtocolCommand(ObdProtocols.AUTO).run(socket.getInputStream(), socket.getOutputStream());
-            //Creating a new thread for the obd to update too
-            Thread thread = new Thread();
-            //Changed 5-2-2017
-            RPMCommand rpmCommand = new RPMCommand();
-            WidebandAirFuelRatioCommand airFuelCommand = new WidebandAirFuelRatioCommand();
-            FuelLevelCommand fuelCommand = new FuelLevelCommand();
-            AirIntakeTemperatureCommand aitCommand = new AirIntakeTemperatureCommand();
-            BarometricPressureCommand baroPress = new BarometricPressureCommand();
-            airFuelCommand.run(socket.getInputStream(), socket.getOutputStream());
-
-            while (!thread.currentThread().isInterrupted()) {
-
-                rpmCommand.run(socket.getInputStream(), socket.getOutputStream());
-                aitCommand.run(socket.getInputStream(), socket.getOutputStream());
-                fuelCommand.run(socket.getInputStream(), socket.getOutputStream());
-                Log.d("DEBUG_0", "RPM: " + rpmCommand.getFormattedResult());
-                Log.d("DEBUG_1", "Air Fuel: " + airFuelCommand.getFormattedResult());
-                Log.d("DEBUG_2", "IAT: " + aitCommand.getFormattedResult());
-                Log.d("DEBUG_3", "Boost/Vac: " + baroPress.getFormattedResult());
-
-                airFuelEdit.setText(airFuelCommand.getFormattedResult());
-                rpmEdit.setText(airFuelCommand.getFormattedResult());
-                fuelEdit.setText(fuelCommand.getFormattedResult());
-                tempEdit.setText(aitCommand.getFormattedResult());
-                pressure.setText(baroPress.getFormattedResult());
-            }
-
+            socket.close();
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
-    }
 
+    }
 
 
 
